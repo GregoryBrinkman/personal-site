e90d2c5c15744695e4ef444eb7c40387842070b2
GregoryBrinkman <gregorylucasbrinkman@siu.edu>
2017-12-22 00:38:18 -0600

    updating routes

--- config/routes.js
+++ config/routes.js
@@ -1,30 +1,11 @@
 const path = require('path');
 const express = require('express');
 
-const home = path.join(__dirname, '../public/index.html');
-const softwarereviews = path.join(__dirname, '../public/reveal.js/demo.html');
-const worldmap = path.join(__dirname, '../public/html/worldmap.html');
-const versusvim = path.join(__dirname, '../public/html/versusvim.html');
+const home = path.join(__dirname, '../public/personal/index.html');
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    app.use('/', express.static('public'));
-    app.use('/images', express.static('public/images'));
+    app.use('/', express.static('public/personal'));
     res.sendFile(home);
-  });
-  app.get('/software-reviews', (req, res) => {
-    app.use( '/software-reviews', express.static('public'));
-    app.use( '/', express.static('public/reveal.js'));
-    res.sendFile(softwarereviews);
-  });
-  app.get('/worldmap', (req, res) => {
-    app.use( '/worldmap', express.static('public/assets'));
-    app.use( '/', express.static('public'));
-    res.sendFile(worldmap);
-  });
-  app.get('/versus-vim', (req, res) => {
-    app.use( '/versus-vim', express.static('public/assets'));
-    app.use( '/', express.static('public'));
-    res.sendFile(versusvim);
   });
 };
