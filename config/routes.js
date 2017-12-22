3af5899c870b581593b8effd4ae8fc0afd5d4e46
GregoryBrinkman <gregorylucasbrinkman@siu.edu>
2017-12-22 00:53:14 -0600

    Improved route, added submodule for project

--- config/routes.js
+++ config/routes.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const express = require('express');
 
-const home = path.join(__dirname, '../public/personal/index.html');
+const home         = path.join(__dirname, '../public/personal/index.html');
+const lifetracker = path.join(__dirname, '../public/life-tracker/dist/index.html');
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
     app.use('/', express.static('public/personal'));
     res.sendFile(home);
+  });
+  app.get('/life-tracker', (req, res) => {
+    app.use('/life-tracker', express.static('public/life-tracker/dist'));
+    app.use('/', express.static('public/life-tracker/dist'));
+    res.sendFile(lifetracker);
   });
 };
