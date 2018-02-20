dde55fa25b2e425453333083b3879f110adf1eeb
Gregory Brinkman <GregoryLucasBrinkman@gmail.com>
2018-02-19 19:15:49 -0600

    Update project, add significant javascript, remove useless files

--- public/personal/js/main.js
+++ public/personal/js/main.js
@@ -1,1 +1,8 @@
-  console.log('I\'m here!');
+//calling all functions
+makeCopyrightDate();
+
+//functions
+function makeCopyrightDate() {
+  let el = document.querySelector('#cr-date');
+  el.innerHTML = (new Date()).getFullYear();
+}
